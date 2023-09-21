974c11f173fc95c7327727eda070d47d291c906f
sik2 <devpang20@gmail.com>
2023-09-21 21:18:21 +0900

    PosterItem 만들기

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components"
 import { movies } from "../util/test_data"
 import { Carousel } from "react-responsive-carousel"
 import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import PosterItem from "../Components/PosterItem";
 
 
 export default function Home () {
@@ -16,10 +17,7 @@ export default function Home () {
                     transtionTime={3}    
                 >
                     {movies.results.map((movie) => (
-                        <ul key={movie.id}>
-                            <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt="" />
-                            <li>{movie.title}</li>
-                        </ul>
+                        <PosterItem key={movie.id} movie={movie} />
                     ))}
                 </Carousel>
             </MoviePoster>
