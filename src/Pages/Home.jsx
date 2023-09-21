25da63df5564375ca490f81e32065ebf6894ed44
sik2 <devpang20@gmail.com>
2023-09-21 21:00:37 +0900

    메인 꾸미기

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,15 +1,27 @@
 import styled from "styled-components"
 import { movies } from "../util/test_data"
+import { Carousel } from "react-responsive-carousel"
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+
 
 export default function Home () {
     return (
         <Container>
             <MoviePoster>
-                {movies.results.map((movie) => (
-                    <ul key={movie.id}>
-                        <li>{movie.title}</li>
-                    </ul>
-                ))}
+                <Carousel
+                    showThumbs
+                    autoPlay
+                    infiiteLoop
+                    showStatus={false}
+                    transtionTime={3}    
+                >
+                    {movies.results.map((movie) => (
+                        <ul key={movie.id}>
+                            <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt="" />
+                            <li>{movie.title}</li>
+                        </ul>
+                    ))}
+                </Carousel>
             </MoviePoster>
         </Container>
     )
