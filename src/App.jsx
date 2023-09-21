be4a67889d13ec654071dcc22daa205c89a4ff58
sik2 <devpang20@gmail.com>
2023-09-21 20:50:41 +0900

    헤더작업

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./Pages/Home"
 import Moive from "./Pages/Movie"
+import Header from "./Components/Header"
 
 function App() {
 
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route index element={<Home />} />
         <Route path="movie" element={<Moive />}/>
---
+++ src/Components/Header.jsx
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom"
+import styled from "styled-components"
+
+export default function Header () {
+    return (
+        <Container>
+            <Cell className="left">
+                <Link to="/">
+                    <Img src={logoImg} alt="로고" />
+                </Link>
+                <Link to="/popular">Popular</Link>
+                <Link to="/top_rated">Top rated</Link>
+                <Link to="/ppcoming">Upcoming</Link>
+            </Cell>
+        </Container>
+    )
+} 
+
+const logoImg = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/IMDb_Logo_Square.svg/128px-IMDb_Logo_Square.svg.png?20200218171646"
+
+const Container = styled.div`
+  margin: 0 2.5rem;
+  padding: 0.5rem 0;
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+`
+
+const Cell = styled.div`
+  display: flex;
+  align-items: center;
+  &.left {
+    gap: 3rem;
+    font-size: 1.3rem;
+    cursor: pointer;
+  }
+`
+
+const Img = styled.img`
+  width: 80px;
+  cursor: pointer;
+  display: block;
+`
